28ac4f1269251fedccfc4d1b1da850d8275ffdab
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-05-31 08:38:56 +0200

    Adds first test + infrastructure for NOT and OR
    Fixes

---
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildNotOrTest.java
@@ -0,0 +1,35 @@
+package test.suite.gipsl.all.build;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.ilp.ILPSolverStatus;
+import org.junit.jupiter.api.Test;
+
+import gipsl.all.build.notor.connector.NotOrConnector;
+
+public class GipslAllBuildNotOrTest extends AGipslAllBuildTest {
+
+	// Setup method
+
+	public void callableSetUp() {
+		gen.persistModel(MODEL_PATH);
+		con = new NotOrConnector(MODEL_PATH);
+	}
+
+	// Actual tests
+
+	@Test
+	public void testMap2to1() {
+		gen.genSubstrateNode("s1", 1);
+		gen.genVirtualNode("v1", 2);
+		gen.genVirtualNode("v2", 2);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(2, ret.objectiveValue());
+	}
+
+}
