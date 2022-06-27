ff544d4d6e2dcea6aef164ed540dac7fcc29f93b
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-06-27 11:09:47 +0200

    Adapts NOT test with obj maximization
    The test currently fails.

--- test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildNotOrTest.java
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildNotOrTest.java
@@ -7,7 +7,6 @@ import org.emoflon.gips.core.ilp.ILPSolverStatus;
 import org.junit.jupiter.api.Test;
 
 import gipsl.all.build.notor.connector.NotOrConnector;
-import test.suite.gips.utils.GipsTestUtils;
 
 public class GipslAllBuildNotOrTest extends AGipslAllBuildTest {
 
@@ -23,18 +22,14 @@ public class GipslAllBuildNotOrTest extends AGipslAllBuildTest {
 	@Test
 	public void testMap2to1() {
 		gen.genSubstrateNode("s1", 1);
-//		gen.genVirtualNode("v1", 1);
-		// TODO!
-//		gen.genVirtualNode("v2", 1);
+		gen.genVirtualNode("v1", 1);
 		callableSetUp();
 
 		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
 
 		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
-		assertEquals(2, ret.objectiveValue());
-
-		// TODO
-		GipsTestUtils.failNotImplemented();
+		// v1 must not be mapped because it is forbidden by a constraint
+		assertEquals(0, ret.objectiveValue());
 	}
 
 }
