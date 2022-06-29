436fc80a0a5727a822f973a1ea04afd7faa489fb
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-06-29 09:43:41 +0200

    Adds AND tests

--- test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildAndTest.java
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildAndTest.java
@@ -1,9 +1,12 @@
 package test.suite.gipsl.all.build;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.ilp.ILPSolverStatus;
 import org.junit.jupiter.api.Test;
 
 import gipsl.all.build.and.connector.AndConnector;
-import test.suite.gips.utils.GipsTestUtils;
 
 public class GipslAllBuildAndTest extends AGipslAllBuildTest {
 
@@ -15,21 +18,75 @@ public class GipslAllBuildAndTest extends AGipslAllBuildTest {
 	}
 
 	// Actual tests
+	// Positive tests
 
 	@Test
 	public void testMap2to1() {
-//		gen.genSubstrateNode("s1", 2);
-//		gen.genVirtualNode("v1", 1);
-//		gen.genVirtualNode("v2", 1);
-//		callableSetUp();
-//
-//		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
-//
-//		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
-//		assertEquals(2, ret.objectiveValue());
+		gen.genSubstrateNode("s1", 10);
+		gen.genVirtualNode("v1", 1);
+		gen.genVirtualNode("v2", 1);
+		callableSetUp();
 
-		// TODO
-		GipsTestUtils.failNotImplemented();
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		// All mappings must be chosen, according to the objective function
+		assertEquals(2, ret.objectiveValue());
+	}
+
+	@Test
+	public void testMap3to1() {
+		gen.genSubstrateNode("s1", 10);
+		gen.genVirtualNode("v1", 1);
+		gen.genVirtualNode("v2", 1);
+		gen.genVirtualNode("v3", 1);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		// All mappings must be chosen, according to the objective function
+		assertEquals(3, ret.objectiveValue());
+	}
+
+	@Test
+	public void testMap4to1() {
+		gen.genSubstrateNode("s1", 10);
+		gen.genVirtualNode("v1", 1);
+		gen.genVirtualNode("v2", 1);
+		gen.genVirtualNode("v3", 1);
+		gen.genVirtualNode("leftout", 1);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		// Only 3 mappings must be chosen, according to the constraint
+		assertEquals(3, ret.objectiveValue());
+	}
+
+	// Negative tests
+
+	@Test
+	public void testMap0to1() {
+		gen.genSubstrateNode("s1", 10);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		// No match found -> Problem must be infeasible
+		assertEquals(ILPSolverStatus.INFEASIBLE, ret.status());
+	}
+
+	@Test
+	public void testMap1to0() {
+		gen.genVirtualNode("v1", 1);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		// No match found -> Problem must be infeasible
+		assertEquals(ILPSolverStatus.INFEASIBLE, ret.status());
 	}
 
 }
