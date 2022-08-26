165b6540972c1e2e1e7c73f4453c8f4dff5ebaec
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-08-26 14:03:53 +0200

    Extends ILP sort tests

--- test.suite.gips/src/test/suite/gips/sort/GipsSortSimpleTest.java
+++ test.suite.gips/src/test/suite/gips/sort/GipsSortSimpleTest.java
@@ -105,30 +105,31 @@ public class GipsSortSimpleTest {
 	}
 
 	// Actual tests
+	// Already ordered
 
 	@Test
-	public void test2Entries() {
+	public void test2EntriesNormal() {
 		genNEntries(2);
 		callableSetUp();
 		runAndVerifyResult(2);
 	}
 
 	@Test
-	public void test3Entries() {
+	public void test3EntriesNormal() {
 		genNEntries(3);
 		callableSetUp();
 		runAndVerifyResult(3);
 	}
 
 	@Test
-	public void test10Entries() {
+	public void test10EntriesNormal() {
 		genNEntries(10);
 		callableSetUp();
 		runAndVerifyResult(10);
 	}
 
 	@Test
-	public void test100Entries() {
+	public void test100EntriesNormal() {
 		genNEntries(100);
 		callableSetUp();
 		runAndVerifyResult(100);
@@ -136,15 +137,105 @@ public class GipsSortSimpleTest {
 
 	@Test
 	@Disabled // Runtime
-	public void test1000Entries() {
+	public void test1000EntriesNormal() {
 		genNEntries(1000);
 		callableSetUp();
 		runAndVerifyResult(1000);
 	}
 
+	// Reverse ordered
+
+	@Test
+	public void test2EntriesReverse() {
+		genNEntriesReverse(2);
+		callableSetUp();
+		runAndVerifyResult(2);
+	}
+
+	@Test
+	public void test3EntriesReverse() {
+		genNEntriesReverse(3);
+		callableSetUp();
+		runAndVerifyResult(3);
+	}
+
+	@Test
+	public void test10EntriesReverse() {
+		genNEntriesReverse(10);
+		callableSetUp();
+		runAndVerifyResult(10);
+	}
+
+	@Test
+	public void test100EntriesReverse() {
+		genNEntriesReverse(100);
+		callableSetUp();
+		runAndVerifyResult(100);
+	}
+
+	@Test
+	@Disabled // Runtime
+	public void test1000EntriesReverse() {
+		genNEntriesReverse(1000);
+		callableSetUp();
+		runAndVerifyResult(1000);
+	}
+
+	// Non-ordered
+
+	@Test
+	public void test10EntriesNonOrdered() {
+		genEntriesFromArray(new int[] { 0, 1, 4, 6, 9, 10, 2345, 2354566, 7, 5 });
+		callableSetUp();
+		runAndVerifyResult(10);
+	}
+
+	@Test
+	public void test20EntriesNonOrdered() {
+		genEntriesFromArray(new int[] { 411, 333, 756, 941, 776, 40, 507, 966, 862, 790, 695, 997, 459, 439, 151, 759,
+				91, 652, 753, 984 });
+		callableSetUp();
+		runAndVerifyResult(20);
+	}
+
+	@Test
+	public void test100EntriesNonOrdered() {
+		genEntriesFromArray(new int[] { 482, 566, 335, 66, 316, 230, 588, 271, 551, 884, 829, 643, 246, 842, 564, 669,
+				679, 862, 528, 800, 362, 678, 982, 871, 819, 695, 752, 433, 443, 386, 728, 167, 508, 612, 293, 545, 750,
+				166, 132, 311, 905, 940, 878, 758, 499, 275, 107, 92, 608, 585, 338, 244, 879, 198, 18, 493, 991, 950,
+				846, 824, 16, 606, 374, 204, 400, 345, 419, 732, 666, 253, 491, 52, 601, 478, 488, 195, 892, 266, 242,
+				949, 219, 69, 70, 709, 19, 441, 94, 655, 813, 227, 973, 429, 300, 128, 36, 319, 307, 723, 567, 733 });
+		callableSetUp();
+		runAndVerifyResult(100);
+	}
+
+	// Others
+
+	@Test
+	@Disabled // Currently not supported
+	public void test20EntriesNonOrderedWithDuplicates() {
+		genEntriesFromArray(new int[] { 6, 2, 1, 8, 10, 7, 9, 3, 4, 5, 1, 7, 9, 2, 8, 10, 6, 4, 5, 3 });
+		callableSetUp();
+		runAndVerifyResult(20);
+	}
+
+	// Utility methods
+
 	private void genNEntries(final int n) {
 		for (int i = 1; i <= n; i++) {
 			genEntry(i);
+		}
+	}
+
+	private void genNEntriesReverse(final int n) {
+		for (int i = n - 1; i >= 0; i--) {
+			genEntry(i);
+		}
+	}
+
+	private void genEntriesFromArray(final int[] entries) {
+		for (int i = 0; i < entries.length; i++) {
+			genEntry(entries[i]);
 		}
 	}
 
