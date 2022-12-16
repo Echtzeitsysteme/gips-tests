fec3ece0af387fc17410f1ffad468a58d4925288
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-12-16 09:07:48 +0100

    Adds first simple tests for mapping access in objectives

---
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildObjectiveMappingTest.java
@@ -0,0 +1,72 @@
+package test.suite.gipsl.all.build;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.ilp.ILPSolverStatus;
+import org.junit.jupiter.api.Test;
+
+import gipsl.all.build.objectivemapping.connector.ObjectiveMappingConnector;
+
+public class GipslAllBuildObjectiveMappingTest extends AGipslAllBuildTest {
+
+	// Setup method
+
+	public void callableSetUp() {
+		gen.persistModel(MODEL_PATH);
+		con = new ObjectiveMappingConnector(MODEL_PATH);
+	}
+
+	// Actual tests
+
+	@Test
+	public void testMap1to1Res1() {
+		gen.genSubstrateNode("s1", 1);
+		gen.genVirtualNode("v1", 1);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(1, ret.objectiveValue());
+	}
+
+	@Test
+	public void testMap1to1Res2() {
+		gen.genSubstrateNode("s1", 1);
+		gen.genVirtualNode("v1", 2);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(2, ret.objectiveValue());
+	}
+
+	@Test
+	public void testMap2to1Res1() {
+		gen.genSubstrateNode("s1", 2);
+		gen.genVirtualNode("v1", 1);
+		gen.genVirtualNode("v2", 1);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(2, ret.objectiveValue());
+	}
+
+	@Test
+	public void testMap2to1Res3() {
+		gen.genSubstrateNode("s1", 2);
+		gen.genVirtualNode("v1", 3);
+		gen.genVirtualNode("v2", 3);
+		callableSetUp();
+
+		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+
+		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(6, ret.objectiveValue());
+	}
+
+}
