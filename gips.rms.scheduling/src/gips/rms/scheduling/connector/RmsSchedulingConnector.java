c89c0238096b80b97567c22eead0d3b179b3d993
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-04-05 11:28:09 +0200

    Adds a metamodel + a simple GIPSL specification to check the CTT

---
+++ gips.rms.scheduling/src/gips/rms/scheduling/connector/RmsSchedulingConnector.java
@@ -0,0 +1,26 @@
+package gips.rms.scheduling.connector;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+
+import gips.rms.scheduling.api.gips.SchedulingGipsAPI;
+import test.suite.gips.utils.AConnector;
+import test.suite.gips.utils.GipsTestUtils;
+import test.suite.gips.utils.GlobalTestConfig;
+
+public class RmsSchedulingConnector extends AConnector {
+
+	public RmsSchedulingConnector(final String modelPath) {
+		api = new SchedulingGipsAPI();
+		api.init(GipsTestUtils.pathToAbsUri(modelPath));
+		GlobalTestConfig.overrideSolver(api);
+	}
+
+	@Override
+	public ILPSolverOutput run(final String outputPath) {
+		final ILPSolverOutput output = solve();
+		final var ams = ((SchedulingGipsAPI) api).getT2s().applyNonZeroMappings();
+		save(outputPath);
+		return output;
+	}
+
+}
