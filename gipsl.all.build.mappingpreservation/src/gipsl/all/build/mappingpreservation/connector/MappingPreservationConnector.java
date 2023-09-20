fe6ec94d8a7a265e760ff7a7d146e990e4dc632d
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-09-20 11:57:31 +0200

    Extends test case to use getNonZeroVariableMappings

--- gipsl.all.build.mappingpreservation/src/gipsl/all/build/mappingpreservation/connector/MappingPreservationConnector.java
+++ gipsl.all.build.mappingpreservation/src/gipsl/all/build/mappingpreservation/connector/MappingPreservationConnector.java
@@ -34,10 +34,24 @@ public class MappingPreservationConnector extends AConnector {
 	}
 	
 	public List<Optional<MapVnodeMatch>> applyMappingWithVnodeName(final String vnodeName) {
-		final var mappings = ((MappingpreservationGipsAPI) api).getN2n().getMappings();
-		final var filtered = mappings.values().stream().filter(t -> {
+//		final var mappings = ((MappingpreservationGipsAPI) api).getN2n().getMappings();
+//		final var filtered = mappings.values().stream().filter(t -> {
+//			return t.getMatch().getVnode().getName().equals(vnodeName);
+//		}).toList();
+		final var mappings = ((MappingpreservationGipsAPI) api).getN2n().getNonZeroVariableMappings();
+		final var filtered = mappings.stream().filter(t -> {
 			return t.getMatch().getVnode().getName().equals(vnodeName);
 		}).toList();
+		
+		// Check that only one mapping should be applied
+		if (filtered.size() != 1) {
+			throw new UnsupportedOperationException();
+		}
+		
+		// Check if mapping to be applied has a value > 0
+		if (!(filtered.get(0).getValue() > 0)) {
+			throw new InternalError();
+		}
 		
 		final var rule = ((MappingpreservationGipsAPI) api).getN2n().getGTRule();
 		return filtered.stream().map(m -> rule.apply(m.getMatch(), true)).collect(Collectors.toList());
