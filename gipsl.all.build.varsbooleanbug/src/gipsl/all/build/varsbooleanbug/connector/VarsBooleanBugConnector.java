aa2b09aeea0020ea297b694c11a0258894e7942b
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-11-21 15:55:56 +0100

    Adds an executable test for the GIPSL specification

---
+++ gipsl.all.build.varsbooleanbug/src/gipsl/all/build/varsbooleanbug/connector/VarsBooleanBugConnector.java
@@ -0,0 +1,33 @@
+package gipsl.all.build.varsbooleanbug.connector;
+
+import java.util.Map;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+
+import gipsl.all.build.varsbooleanbug.api.gips.VarsbooleanbugGipsAPI;
+import gipsl.all.build.varsbooleanbug.api.gips.mapping.N2nMapping;
+import test.suite.gips.utils.AConnector;
+import test.suite.gips.utils.GipsTestUtils;
+import test.suite.gips.utils.GlobalTestConfig;
+
+public class VarsBooleanBugConnector extends AConnector {
+
+	public VarsBooleanBugConnector(final String modelPath) {
+		api = new VarsbooleanbugGipsAPI();
+		api.init(GipsTestUtils.pathToAbsUri(modelPath));
+		GlobalTestConfig.overrideSolver(api);
+	}
+
+	@Override
+	public ILPSolverOutput run(final String outputPath) {
+		final ILPSolverOutput output = solve();
+		((VarsbooleanbugGipsAPI) api).getN2n().applyNonZeroMappings();
+		save(outputPath);
+		return output;
+	}
+	
+	public Map<String, N2nMapping> getN2nMappings() {
+		return ((VarsbooleanbugGipsAPI) api).getN2n().getMappings();
+	}
+
+}
