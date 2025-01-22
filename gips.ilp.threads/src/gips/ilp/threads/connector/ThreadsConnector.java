2d3dcb26545ed1d4ae14ad9fc45acc29172e50b7
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-01-22 13:00:34 +0100

    Adds tests for the API ILP solver thread overwrite method

---
+++ gips.ilp.threads/src/gips/ilp/threads/connector/ThreadsConnector.java
@@ -0,0 +1,51 @@
+package gips.ilp.threads.connector;
+
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+
+import gips.ilp.threads.api.gips.ThreadsGipsAPI;
+import test.suite.gips.utils.AConnector;
+import test.suite.gips.utils.GipsTestUtils;
+import test.suite.gips.utils.GlobalTestConfig;
+
+public class ThreadsConnector extends AConnector {
+
+	public ThreadsConnector(final String modelPath, final int newNumberOfThreads) {
+		this(modelPath);
+		api.setIlpSolverThreads(newNumberOfThreads);
+	}
+
+	public ThreadsConnector(final String modelPath) {
+		api = new ThreadsGipsAPI();
+		api.init(GipsTestUtils.pathToAbsUri(modelPath));
+		GlobalTestConfig.overrideSolver(api);
+	}
+
+	@Override
+	public ILPSolverOutput run(final String outputPath) {
+		final ILPSolverOutput output = solve();
+		((ThreadsGipsAPI) api).getS2t().applyNonZeroMappings();
+		save(outputPath);
+		return output;
+	}
+
+	/*
+	 * Setters for the test implementation.
+	 */
+
+	public void setSolverConfigThreads(final int value) {
+		api.setIlpSolverThreads(value);
+	}
+
+	/*
+	 * Getters for the test implementation.
+	 */
+
+	public int getSolverConfigThreads() {
+		return api.getSolverConfig().threads();
+	}
+
+	public boolean getSolverConfigThreadsEnabled() {
+		return api.getSolverConfig().threadCount();
+	}
+
+}
