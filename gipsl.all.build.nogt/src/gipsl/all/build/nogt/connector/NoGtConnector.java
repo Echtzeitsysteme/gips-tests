411c74a6b32a0380f339e0d844b0e94840722076
SebastianE <sebastian.ehmes@gmail.com>
2025-01-26 01:51:54 +0100

    + changed the build.nogt test specification to fit new grammar

--- gipsl.all.build.nogt/src/gipsl/all/build/nogt/connector/NoGtConnector.java
+++ gipsl.all.build.nogt/src/gipsl/all/build/nogt/connector/NoGtConnector.java
@@ -1,6 +1,6 @@
 package gipsl.all.build.nogt.connector;
 
-import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.milp.SolverOutput;
 
 import gipsl.all.build.nogt.api.gips.NogtGipsAPI;
 import test.suite.gips.utils.AConnector;
@@ -16,8 +16,8 @@ public class NoGtConnector extends AConnector {
 	}
 
 	@Override
-	public ILPSolverOutput run(final String outputPath) {
-		final ILPSolverOutput output = solve();
+	public SolverOutput run(final String outputPath) {
+		final SolverOutput output = solve();
 		// No matches/mappers to apply
 		return output;
 	}
