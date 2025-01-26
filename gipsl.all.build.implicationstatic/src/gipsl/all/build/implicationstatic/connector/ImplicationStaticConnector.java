a7fdad87ee2d1dca584b591f0d489cf06e8d1a6b
SebastianE <sebastian.ehmes@gmail.com>
2025-01-26 01:48:09 +0100

    + changed the build.implicationstatic test specification to fit new grammar

--- gipsl.all.build.implicationstatic/src/gipsl/all/build/implicationstatic/connector/ImplicationStaticConnector.java
+++ gipsl.all.build.implicationstatic/src/gipsl/all/build/implicationstatic/connector/ImplicationStaticConnector.java
@@ -1,6 +1,6 @@
 package gipsl.all.build.implicationstatic.connector;
 
-import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.milp.SolverOutput;
 
 import gipsl.all.build.implicationstatic.api.gips.ImplicationstaticGipsAPI;
 import test.suite.gips.utils.AConnector;
@@ -16,8 +16,8 @@ public class ImplicationStaticConnector extends AConnector {
 	}
 
 	@Override
-	public ILPSolverOutput run(final String outputPath) {
-		final ILPSolverOutput output = solve();
+	public SolverOutput run(final String outputPath) {
+		final SolverOutput output = solve();
 		// No mappings/matches to apply
 		return output;
 	}
