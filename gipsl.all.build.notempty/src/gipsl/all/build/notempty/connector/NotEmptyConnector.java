50fb48190dbb8ec6340a385f95a5d75bc8920a5d
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-03-21 10:31:55 +0100

    Adjustments after merge: update to GIPSL v1.5 + renaming of classes

--- gipsl.all.build.notempty/src/gipsl/all/build/notempty/connector/NotEmptyConnector.java
+++ gipsl.all.build.notempty/src/gipsl/all/build/notempty/connector/NotEmptyConnector.java
@@ -1,6 +1,6 @@
 package gipsl.all.build.notempty.connector;
 
-import org.emoflon.gips.core.ilp.ILPSolverOutput;
+import org.emoflon.gips.core.milp.SolverOutput;
 
 import gipsl.all.build.notempty.api.gips.NotemptyGipsAPI;
 import test.suite.gips.utils.AConnector;
@@ -14,8 +14,8 @@ public class NotEmptyConnector extends AConnector {
 	}
 
 	@Override
-	public ILPSolverOutput run(final String outputPath) {
-		final ILPSolverOutput output = solve();
+	public SolverOutput run(final String outputPath) {
+		final SolverOutput output = solve();
 		((NotemptyGipsAPI) api).getN2n().applyNonZeroMappings();
 		return output;
 	}
--- test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildNotEmptyTest.java
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildNotEmptyTest.java
@@ -2,10 +2,9 @@ package test.suite.gipsl.all.build;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.emoflon.gips.core.ilp.ILPSolverOutput;
-import org.emoflon.gips.core.ilp.ILPSolverStatus;
+import org.emoflon.gips.core.milp.SolverOutput;
+import org.emoflon.gips.core.milp.SolverStatus;
 import org.junit.jupiter.api.Test;
-
 import gipsl.all.build.notempty.connector.NotEmptyConnector;
 
 public class GipslAllBuildNotEmptyTest extends AGipslAllBuildTest {
@@ -26,9 +25,9 @@ public class GipslAllBuildNotEmptyTest extends AGipslAllBuildTest {
 		gen.genVirtualNode("v1", 1);
 		callableSetUp();
 
-		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+		final SolverOutput ret = con.run(OUTPUT_PATH);
 
-		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		assertEquals(1, Math.abs(ret.objectiveValue()));
 	}
 
@@ -40,9 +39,9 @@ public class GipslAllBuildNotEmptyTest extends AGipslAllBuildTest {
 		gen.genVirtualNode("v2", 1);
 		callableSetUp();
 
-		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+		final SolverOutput ret = con.run(OUTPUT_PATH);
 
-		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		assertEquals(2, Math.abs(ret.objectiveValue()));
 	}
 
@@ -55,9 +54,9 @@ public class GipslAllBuildNotEmptyTest extends AGipslAllBuildTest {
 		gen.genVirtualNode("v2", 1);
 		callableSetUp();
 
-		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+		final SolverOutput ret = con.run(OUTPUT_PATH);
 
-		assertEquals(ILPSolverStatus.OPTIMAL, ret.status());
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		// Both virtual nodes must be mapped to both substrate nodes
 		assertEquals(2 * 2, Math.abs(ret.objectiveValue()));
 	}
@@ -68,9 +67,14 @@ public class GipslAllBuildNotEmptyTest extends AGipslAllBuildTest {
 		gen.genVirtualNode("v1", 1);
 		callableSetUp();
 
-		final ILPSolverOutput ret = con.run(OUTPUT_PATH);
+		final SolverOutput ret = con.run(OUTPUT_PATH);
 
-		assertEquals(ILPSolverStatus.INFEASIBLE, ret.status());
+		assertEquals(SolverStatus.INFEASIBLE, ret.status());
+	}
+
+	@Override
+	public Class<?> getConnectorClass() {
+		return NotEmptyConnector.class;
 	}
 
 }
