c3828ce0dc9707acad47ead98eba592ba513b88f
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2025-11-26 15:37:44 +0100

    Fix project name for joinAll

--- gipsl.all.build.joinall/src/gipsl/all/build/joinall/connector/JoinAllConnector.java
+++ gipsl.all.build.joinall/src/gipsl/all/build/joinall/connector/JoinAllConnector.java
@@ -2,7 +2,7 @@ package gipsl.all.build.joinall.connector;
 
 import org.emoflon.gips.core.milp.SolverOutput;
 
-import gips.all.build.joinall.api.gips.JoinallGipsAPI;
+import gipsl.all.build.joinall.api.gips.JoinallGipsAPI;
 import test.suite.gips.utils.AConnector;
 import test.suite.gips.utils.GipsTestUtils;
 import test.suite.gips.utils.GlobalTestConfig;
