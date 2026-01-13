8fdbea42bbd47cd00e3841b9bcbff327e87245db
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2026-01-13 12:10:12 +0100

    Refactor utility asserts

--- test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildTypeExtensionTest.java
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildTypeExtensionTest.java
@@ -3,19 +3,19 @@ package test.suite.gipsl.all.build;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static test.suite.gips.utils.TextFileAsserts.assertTextContains;
+import static test.suite.gips.utils.TextFileAsserts.assertTextContainsExactly;
+import static test.suite.gips.utils.TextFileAsserts.assertTextContainsNot;
+import static test.suite.gips.utils.TextFileAsserts.deleteFile;
+import static test.suite.gips.utils.TextFileAsserts.readTextFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.emoflon.gips.core.milp.SolverOutput;
 import org.emoflon.gips.core.milp.SolverStatus;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import gipsl.all.build.typeextension.api.gips.TypeextensionGipsAPI;
@@ -40,33 +40,19 @@ public class GipslAllBuildTypeExtensionTest extends AGipslAllBuildTest {
 	}
 
 	public void deleteOutput() {
-		Path path = getLPOutputPath();
-		try {
-			if (Files.exists(path)) {
-				System.out.println("Deleting: " + path);
-				Files.deleteIfExists(path);
-			}
-		} catch (IOException e) {
-			Assertions.fail(e);
-		}
+		deleteFile(getLPOutputPath());
+
 	}
 
 	public List<String> loadLPFile() {
-		Path path = getLPOutputPath();
-		try {
-			List<String> allLines = Files.readAllLines(path);
-			return allLines;
-		} catch (IOException e) {
-			Assertions.fail(e);
-			return null;
-		}
+		return readTextFile(getLPOutputPath());
 	}
 
 	// Actual tests
 
 	@Test
 	public void testMap1to1() {
-		gen.genSubstrateNode("s1", 2);
+		gen.genSubstrateNode("s1", 3);
 		gen.genVirtualNode("v1", 1);
 		callableSetUp();
 
@@ -74,16 +60,20 @@ public class GipslAllBuildTypeExtensionTest extends AGipslAllBuildTest {
 		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		assertEquals(1, Math.abs(ret.objectiveValue()));
 
-		// Ensure that each relevant mapping contains at least one match
+		// Ensure that at least one mapping was created
 		assertFalse(getAPI().getN2n().getMappings().isEmpty());
 
-		assertEquals(1, // one substrate node in model -> 1 extension expected
+		assertEquals(1, // 1 SubstrateNode in model -> 1 extension expected
 				getSubstrateNodeExtensions().size());
-		assertEquals(1, // one substrate node in model -> 1 extension expected
+		assertEquals(1, // 1 SubstrateResourceNode in model -> 1 extension expected
 				getSubstrateResourceNodeExtensions().size());
 
-//		assertEquals(1, //
-//				getSubstrateResourceNodeExtension("s1").getValueOfResourceAmountUsed());
+		assertEquals(1, // v1 should be embedded on s1
+				getSubstrateNodeExtension("s1").getValueOfEmbeddedVirtualNodes());
+		assertEquals(1, // v1 needs 1 unit
+				getSubstrateResourceNodeExtension("s1").getValueOfResourceAmountUsed());
+		assertEquals(2, // 2 of 3 should be still available
+				getSubstrateResourceNodeExtension("s1").getValueOfResourceAmountAvailable());
 
 		List<String> lpFile = loadLPFile();
 		assertTextContains(lpFile, Pattern.compile("embeddedVirtualNodes"));
@@ -92,27 +82,37 @@ public class GipslAllBuildTypeExtensionTest extends AGipslAllBuildTest {
 		assertTextContainsNot(lpFile, Pattern.compile("unusedVar"));
 	}
 
-//	@Test
+	@Test
 	public void testMap2to1() {
-		gen.genSubstrateNode("s1", 2);
+		gen.genSubstrateNode("s1", 3);
 		gen.genVirtualNode("v1", 1);
 		gen.genVirtualNode("v2", 1);
 		callableSetUp();
 
 		final SolverOutput ret = con.run(OUTPUT_PATH);
-
 		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		assertEquals(2, Math.abs(ret.objectiveValue()));
 
-		// Ensure that each relevant mapping contains at least one match
-		assertFalse(((UnusedvarsConnector) con).getAPI().getN2n().getMappings().isEmpty());
-		assertFalse(((UnusedvarsConnector) con).getAPI().getUnusedMapping().getMappings().isEmpty());
+		// Ensure that at least one mapping was created
+		assertFalse(getAPI().getN2n().getMappings().isEmpty());
+
+		assertEquals(1, // 1 SubstrateNode in model -> 1 extension expected
+				getSubstrateNodeExtensions().size());
+		assertEquals(1, // 1 SubstrateResourceNode in model -> 1 extension expected
+				getSubstrateResourceNodeExtensions().size());
+
+		assertEquals(2, // v1 and v2 should be embedded on s1
+				getSubstrateNodeExtension("s1").getValueOfEmbeddedVirtualNodes());
+		assertEquals(2, // v1 needs 1 unit and v2 needs 1 unit
+				getSubstrateResourceNodeExtension("s1").getValueOfResourceAmountUsed());
+		assertEquals(1, // 1 of 3 should be still available
+				getSubstrateResourceNodeExtension("s1").getValueOfResourceAmountAvailable());
 
 		List<String> lpFile = loadLPFile();
-		assertTextContainsExactly(lpFile, Pattern.compile("\\bn2n#\\d+\\b(?!->)"), 6);
-		assertTextContainsExactly(lpFile, Pattern.compile("\\bn2n#\\d+->usedVar\\b"), 4);
-		assertTextContainsNot(lpFile, Pattern.compile("\\bn2n#\\d+->unusedVar\\b"));
-		assertTextContainsNot(lpFile, Pattern.compile("unusedMapping"));
+		assertTextContains(lpFile, Pattern.compile("embeddedVirtualNodes"));
+		assertTextContains(lpFile, Pattern.compile("resourceAmountAvailable"));
+		assertTextContains(lpFile, Pattern.compile("resourceAmountUsed"));
+		assertTextContainsNot(lpFile, Pattern.compile("unusedVar"));
 	}
 
 //	@Test
@@ -140,46 +140,6 @@ public class GipslAllBuildTypeExtensionTest extends AGipslAllBuildTest {
 	}
 
 	// Utility methods
-
-	private static void assertTextContainsNot(List<String> lines, Pattern regex) {
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			if (m.find()) {
-				String msg = String.format("Found pattern '%s' in line %d, character %d as '%s'", regex, i + 1,
-						m.start(), m.group());
-				Assertions.fail(msg);
-			}
-		}
-	}
-
-	private static void assertTextContains(List<String> lines, Pattern regex) {
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			if (m.find()) {
-				return;
-			}
-		}
-
-		Assertions.fail(String.format("Unable to find pattern '%s'", regex));
-	}
-
-	private void assertTextContainsExactly(List<String> lines, Pattern regex, int exactly) {
-		List<Integer> counter = new LinkedList<>();
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			while (m.find()) {
-				counter.add(i);
-			}
-		}
-
-		if (counter.size() == 0) {
-			Assertions.fail(String.format("Unable to find pattern '%s'", regex));
-		} else if (counter.size() != exactly) {
-			String msg = String.format("Found pattern '%s' %d times, expected to find it %d times", regex,
-					counter.size(), exactly);
-			Assertions.fail(msg);
-		}
-	}
 
 	private TypeextensionGipsAPI getAPI() {
 		return ((TypeextensionConnector) con).getAPI();
--- test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildUnusedVarsTest.java
+++ test.suite.gips/src/test/suite/gipsl/all/build/GipslAllBuildUnusedVarsTest.java
@@ -3,18 +3,17 @@ package test.suite.gipsl.all.build;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static test.suite.gips.utils.TextFileAsserts.assertTextContainsExactly;
+import static test.suite.gips.utils.TextFileAsserts.assertTextContainsNot;
+import static test.suite.gips.utils.TextFileAsserts.deleteFile;
+import static test.suite.gips.utils.TextFileAsserts.readTextFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.emoflon.gips.core.milp.SolverOutput;
 import org.emoflon.gips.core.milp.SolverStatus;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import gipsl.all.build.unusedvars.connector.UnusedvarsConnector;
@@ -35,26 +34,12 @@ public class GipslAllBuildUnusedVarsTest extends AGipslAllBuildTest {
 	}
 
 	public void deleteOutput() {
-		Path path = getLPOutputPath();
-		try {
-			if (Files.exists(path)) {
-				System.out.println("Deleting: " + path);
-				Files.deleteIfExists(path);
-			}
-		} catch (IOException e) {
-			Assertions.fail(e);
-		}
+		deleteFile(getLPOutputPath());
+
 	}
 
 	public List<String> loadLPFile() {
-		Path path = getLPOutputPath();
-		try {
-			List<String> allLines = Files.readAllLines(path);
-			return allLines;
-		} catch (IOException e) {
-			Assertions.fail(e);
-			return null;
-		}
+		return readTextFile(getLPOutputPath());
 	}
 
 	// Actual tests
@@ -126,48 +111,6 @@ public class GipslAllBuildUnusedVarsTest extends AGipslAllBuildTest {
 		assertTextContainsExactly(lpFile, Pattern.compile("\\bn2n#\\d+->usedVar\\b"), 20);
 		assertTextContainsNot(lpFile, Pattern.compile("\\bn2n#\\d+->unusedVar\\b"));
 		assertTextContainsNot(lpFile, Pattern.compile("unusedMapping"));
-	}
-
-	// Utility methods
-
-	private static void assertTextContainsNot(List<String> lines, Pattern regex) {
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			if (m.find()) {
-				String msg = String.format("Found pattern '%s' in line %d, character %d as '%s'", regex, i + 1,
-						m.start(), m.group());
-				Assertions.fail(msg);
-			}
-		}
-	}
-
-	private static void assertTextContains(List<String> lines, Pattern regex) {
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			if (m.find()) {
-				return;
-			}
-		}
-
-		Assertions.fail(String.format("Unable to find pattern '%s'", regex));
-	}
-
-	private void assertTextContainsExactly(List<String> lines, Pattern regex, int exactly) {
-		List<Integer> counter = new LinkedList<>();
-		for (int i = 0; i < lines.size(); ++i) {
-			Matcher m = regex.matcher(lines.get(i));
-			while (m.find()) {
-				counter.add(i);
-			}
-		}
-
-		if (counter.size() == 0) {
-			Assertions.fail(String.format("Unable to find pattern '%s'", regex));
-		} else if (counter.size() != exactly) {
-			String msg = String.format("Found pattern '%s' %d times, expected to find it %d times", regex,
-					counter.size(), exactly);
-			Assertions.fail(msg);
-		}
 	}
 
 	@Override
