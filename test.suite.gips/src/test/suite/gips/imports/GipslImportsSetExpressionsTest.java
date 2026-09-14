f1ee6bbe461ebfde61fa2043765501d2b1ecf166
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2026-09-14 15:02:34 +0200

    Fix test for scalar attributes

--- test.suite.gips/src/test/suite/gips/imports/GipslImportsSetExpressionsTest.java
+++ test.suite.gips/src/test/suite/gips/imports/GipslImportsSetExpressionsTest.java
@@ -2,11 +2,16 @@ package test.suite.gips.imports;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Collection;
+import java.util.stream.Stream;
+
 import org.emoflon.gips.core.milp.SolverOutput;
 import org.emoflon.gips.core.milp.SolverStatus;
 import org.junit.jupiter.api.Test;
 
 import gipsl.imports.scalarattributes.setexpressions.api.gips.SetexpressionsGipsAPI;
+import gipsl.imports.scalarattributes.setexpressions.api.gips.mapping.Guest2hostMapping;
+import gipsl.imports.scalarattributes.setexpressions.api.gips.types.TypeGuestExtension;
 import gipsl.imports.scalarattributes.setexpressions.connector.SetExpressionsConnector;
 
 public class GipslImportsSetExpressionsTest extends AGipslImportsTest {
@@ -23,77 +28,150 @@ public class GipslImportsSetExpressionsTest extends AGipslImportsTest {
 	// Positive tests
 
 	@Test
-	public void testG2SG1on1Yes() {
+	public void test_G1_H1() {
 		gen.genHost("h1", 8);
-		gen.genSpecialGuest("g3", 1, 1);
+		gen.genGuest("g1", 1);
 		callableSetUp();
 
 		final SolverOutput ret = con.run(OUTPUT_PATH);
 
-		assertEquals(SolverStatus.INFEASIBLE, ret.status());
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
 		assertEquals(1, Math.abs(ret.objectiveValue()));
 
+		assertEquals(1, //
+				getGuestExtension("g1").getValueOfDemandSum());
+
+		assertEquals(0, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(0, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
 	}
 
 	@Test
-	public void testSG3on1Yes() {
+	public void test_G1_SG1_H1() {
 		gen.genHost("h1", 8);
-		gen.genSpecialGuest("g1", 4, 1);
-		gen.genSpecialGuest("g2", 2, 2);
-		gen.genSpecialGuest("g3", 1, 4);
-
-//		gen.addGuestToHost("g1", "h1");
-//		gen.addGuestToHost("g2", "h1");
-//		gen.addGuestToHost("g3", "h1");
+		gen.genGuest("g1", 1);
+		gen.genSpecialGuest("sg1", 3, 2);
 		callableSetUp();
 
 		final SolverOutput ret = con.run(OUTPUT_PATH);
-
 		assertEquals(SolverStatus.OPTIMAL, ret.status());
-		assertEquals(0, Math.abs(ret.objectiveValue()));
+		assertEquals(2, Math.abs(ret.objectiveValue()));
+
+		assertEquals(1, //
+				getGuestExtension("g1").getValueOfDemandSum());
+		assertEquals(3, //
+				getGuestExtension("sg1").getValueOfDemandSum());
+
+		assertEquals(0, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(1, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
 	}
 
 	@Test
-	public void testG2SG3on1Yes() {
-		gen.genHost("h1", 10);
-		gen.genGuest("g1", 4);
-		gen.genGuest("g2", 2);
-		gen.genSpecialGuest("g3", 1, 1);
-		gen.genSpecialGuest("g4", 1, 2);
-		gen.genSpecialGuest("g5", 1, 4);
-
-		gen.addGuestToHost("g1", "h1");
-		gen.addGuestToHost("g2", "h1");
-		gen.addGuestToHost("g3", "h1");
-		gen.addGuestToHost("g4", "h1");
-		gen.addGuestToHost("g5", "h1");
+	public void test_SG3_H1() {
+		gen.genHost("h1", 9);
+		gen.genSpecialGuest("sg1", 2, 2);
+		gen.genSpecialGuest("sg2", 4, 2);
+		gen.genSpecialGuest("sg3", 3, 2);
 		callableSetUp();
 
 		final SolverOutput ret = con.run(OUTPUT_PATH);
-
 		assertEquals(SolverStatus.OPTIMAL, ret.status());
-		assertEquals(0, Math.abs(ret.objectiveValue()));
+		assertEquals(3, Math.abs(ret.objectiveValue()));
+
+		assertEquals(2, //
+				getGuestExtension("sg1").getValueOfDemandSum());
+		assertEquals(4, //
+				getGuestExtension("sg2").getValueOfDemandSum());
+		assertEquals(3, //
+				getGuestExtension("sg3").getValueOfDemandSum());
+
+		assertEquals(0, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(3, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
 	}
 
 	@Test
-	public void testG2SG2On2Yes() {
-		gen.genHost("h1", 6);
-		gen.genHost("h2", 6);
-		gen.genSpecialGuest("g1", 4, 1);
-		gen.genGuest("g2", 2);
-		gen.genSpecialGuest("g3", 1, 2);
-		gen.genGuest("g4", 1);
-
-		gen.addGuestToHost("g1", "h1");
-		gen.addGuestToHost("g2", "h1");
-		gen.addGuestToHost("g3", "h2");
-		gen.addGuestToHost("g4", "h2");
+	public void test_SG3_SH1() {
+		gen.genSpecialHost("sh1", 9, 4);
+		gen.genSpecialGuest("sg1", 2, 2);
+		gen.genSpecialGuest("sg2", 4, 2);
+		gen.genSpecialGuest("sg3", 3, 2);
 		callableSetUp();
 
 		final SolverOutput ret = con.run(OUTPUT_PATH);
-
 		assertEquals(SolverStatus.OPTIMAL, ret.status());
-		assertEquals(0, Math.abs(ret.objectiveValue()));
+		assertEquals(3, Math.abs(ret.objectiveValue()));
+
+		assertEquals(2, //
+				getGuestExtension("sg1").getValueOfDemandSum());
+		assertEquals(4, //
+				getGuestExtension("sg2").getValueOfDemandSum());
+		assertEquals(3, //
+				getGuestExtension("sg3").getValueOfDemandSum());
+
+		assertEquals(3, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(3, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
+	}
+
+	@Test
+	public void test_G2_SG2_SH1() {
+		gen.genSpecialHost("sh1", 9, 4);
+		gen.genGuest("g1", 1);
+		gen.genGuest("g2", 1);
+		gen.genSpecialGuest("sg1", 2, 2);
+		gen.genSpecialGuest("sg2", 4, 2);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(4, Math.abs(ret.objectiveValue()));
+
+		assertEquals(1, //
+				getGuestExtension("g1").getValueOfDemandSum());
+		assertEquals(1, //
+				getGuestExtension("g2").getValueOfDemandSum());
+		assertEquals(2, //
+				getGuestExtension("sg1").getValueOfDemandSum());
+		assertEquals(4, //
+				getGuestExtension("sg2").getValueOfDemandSum());
+
+		assertEquals(4, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(2, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
+	}
+
+	@Test
+	public void test_SG3_SH3() {
+		gen.genSpecialHost("sh1", 5, 4);
+		gen.genSpecialHost("sh2", 5, 4);
+		gen.genSpecialHost("sh3", 5, 4);
+		gen.genSpecialGuest("sg1", 2, 1);
+		gen.genSpecialGuest("sg2", 4, 2);
+		gen.genSpecialGuest("sg3", 3, 4);
+		callableSetUp();
+
+		final SolverOutput ret = con.run(OUTPUT_PATH);
+		assertEquals(SolverStatus.OPTIMAL, ret.status());
+		assertEquals(3, Math.abs(ret.objectiveValue()));
+
+		assertEquals(2, //
+				getGuestExtension("sg1").getValueOfDemandSum());
+		assertEquals(4, //
+				getGuestExtension("sg2").getValueOfDemandSum());
+		assertEquals(3, //
+				getGuestExtension("sg3").getValueOfDemandSum());
+
+		assertEquals(9, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialHost()).count());
+		assertEquals(9, //
+				getG2HMappings().filter(m -> m.getValueOfHasSpecialGuest()).count());
 	}
 
 	@Override
@@ -103,6 +181,23 @@ public class GipslImportsSetExpressionsTest extends AGipslImportsTest {
 
 	private SetexpressionsGipsAPI getAPI() {
 		return ((SetExpressionsConnector) con).getAPI();
+	}
+
+	private Collection<TypeGuestExtension> getGuestExtensions() {
+		return getAPI().getTypeGuest().getExtensions();
+	}
+
+	private Stream<Guest2hostMapping> getG2HMappings() {
+		return getAPI().getGuest2host().getMappings().values().stream();
+	}
+
+	private TypeGuestExtension getGuestExtension(String name) {
+		for (var node : getGuestExtensions()) {
+			if (node.getContext().getName().equals(name)) {
+				return node;
+			}
+		}
+		return null;
 	}
 
 }
